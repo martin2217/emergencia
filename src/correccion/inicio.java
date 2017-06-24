37a9bd6dd4ef8e9d5be6609fcf75e2cff6537b8d
martin2217 <martin_f1g@hotmail.com>
2017-06-23 23:16:41 -0300

    Correccion ortográfica implementada (mejorar el diccionario).. Completar normalización del texto. Completar implementacion del sistema de produccion

---
+++ src/correccion/inicio.java
@@ -0,0 +1,28 @@
+package correccion;
+
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+
+import com.inet.jortho.SpellChecker;
+
+public class inicio {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String cadena1= "te voy a matarr!!! habia una vez un pequeñoo";
+		String cadena2= "dame todaa la plataa, me esta pegandoo!";
+		String cadena3= "me quiere robarr!";
+		JazzySpellChecker corrector = new JazzySpellChecker();
+		
+		cadena1= corrector.getCorrectedText(cadena1);
+		cadena2= corrector.getCorrectedText(cadena2);
+		cadena3= corrector.getCorrectedText(cadena3);
+		
+		
+		System.out.println ("Cadenas ya corregidas: " + cadena1 +
+				", "+cadena2+ ", "+ cadena3);
+	}
+
+}
---
+++ src/correccion/prueba.java
@@ -0,0 +1,64 @@
+package correccion;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JApplet;
+import javax.swing.JEditorPane;
+
+import com.inet.jortho.SpellChecker;
+import com.inet.jortho.FileUserDictionary;
+import com.inet.jortho.WordIterator;
+
+
+
+public class prueba extends JApplet {
+    @Override
+    public void start() {
+ 
+        JEditorPane text = new JEditorPane();
+        text.setText( "This is a simppler textt with spellingg errors." );
+        add( text );
+        
+        
+        // Load the configuration and dictionaries from the current working directory and use the current locale or the first language as default 
+    	//SpellChecker.registerDictionaries( null, null );
+    	// Load the configuration and dictionaries from the sub directory "dict"
+    	/*try {
+			SpellChecker.registerDictionaries( new URL( "file", null, "resources" ), null );
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+    	
+        //FILE LOCATION OF DICTIONARY
+        String userDictionaryPath = "/dictionary/";
+
+        //SET DICTIONARY PROVIDER FROM DICTIONARY PATH
+        SpellChecker.setUserDictionaryProvider(new FileUserDictionary(userDictionaryPath));
+
+        //REGISTER DICTIONARY
+        SpellChecker.registerDictionaries(getClass().getResource(userDictionaryPath), "es");
+		
+
+        SpellChecker.enableAutoSpell(text, true);
+        
+        //SpellChecker.registerDictionaries( null, null);//this.getClass().getResource("/dictionary"),"es" );                
+        SpellChecker.register( text );
+        
+
+        SpellChecker.enableAutoSpell(text, true);
+        
+        WordIterator iterador;
+        /*try {
+			iterador = new WordIterator(text, "comida casa cassa");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+        String aux= "cassa";
+        String aux2 = aux;
+    }
+}
+
+	
