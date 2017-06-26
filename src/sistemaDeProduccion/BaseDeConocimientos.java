7342eb7d4c28887751a815c8e2e0000b47e0a1ed
martin2217 <martin_f1g@hotmail.com>
2017-06-25 22:43:45 -0300

    Correcciones

--- src/sistemaDeProduccion/BaseDeConocimientos.java
+++ src/sistemaDeProduccion/BaseDeConocimientos.java
@@ -13,7 +13,7 @@ public class BaseDeConocimientos {
 	private Set<Regla> reglasActivadas;
 	
 	// Memoria de producciones
-	private HashMap<String, String> palabras;
+	private HashMap<String, String> palabrasAEncontrar;
 	private ArrayList<Regla> reglas;
 	// TODO inicializar con las palabras de las reglas
 	
@@ -25,11 +25,33 @@ public class BaseDeConocimientos {
 	}
 	
 	private void inicializar(){
-		palabras= new LinkedHashMap<String, String>();
+		// TODO Palabras a matchear - COMPLETAR
+		palabrasAEncontrar= new LinkedHashMap<String, String>();
 		reglas=Regla.inicializarReglas();
 	}
 	public void reiniciar(){
-		palabras= new LinkedHashMap<String, String>();
+		//palabrasAEncontrar= new LinkedHashMap<String, String>();
 		palabrasEncontradas = new LinkedHashSet<String>();
+		reglasActivadas = new LinkedHashSet<Regla>();
+	}
+	
+	// Si existe palabra a matchear, la agrega a las palabras encontradas
+	public void addPalabra(String palabra){
+		String aux = palabrasAEncontrar.get(palabra);
+		if(aux != null){
+			palabrasEncontradas.add(aux); // mató/mato, agregaría matar
+		}
+	}
+	public void addPalabras(ArrayList<String> palabras){
+		for(String palabra: palabras){
+			addPalabra(palabra);
+		}
+	}
+	public String stringPalabrasEncontradas(){
+		String retorno="";
+		for(String palabra: palabrasEncontradas){
+			retorno+=palabra+" ";
+		}
+		return retorno;
 	}
 }
